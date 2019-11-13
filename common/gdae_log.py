4d31a059275d6796a4e8d97c08ef08f3eeacdf99
dgvtoken <dgvtoken@163.com>
2019-11-13 10:40:54 +0800

    ZZ
    Revert "first commit"
    
    This reverts commit fe2e6d913bf41f498d67fbb151f25d1037055114.

--- common/gdae_log.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3 python
-# -*- coding: utf-8 -*-
-import logging
-
-from config import gdae_config
-
-logging.basicConfig(**gdae_config.logger_config)
-logger = logging.getLogger()
